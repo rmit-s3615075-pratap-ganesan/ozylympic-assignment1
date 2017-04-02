736cdad1e5b2628deac4a55caab239d9f68b0a81
rmit-s3615564-ashish-bhardwaj <s3615564@student.rmit.edu.au>
2017-04-02 11:48:35 +1000

    exception hadling and comments

--- src/AbstractEvent.java
+++ src/AbstractEvent.java
@@ -1,9 +1,11 @@
 
 public abstract class AbstractEvent implements Event {
-protected int minTime;
-protected int maxTime;
+protected int minTime;         //minimum time of event
+protected int maxTime;         //maximum time of event
 
-
+/*
+ * constructor
+ */
 public AbstractEvent(int minTime, int maxTime) {
 	
 	this.minTime = minTime;
@@ -15,13 +17,17 @@ public int getMinTime(){
 public int getMaxTime(){
 	 return maxTime;
 }
-
+/*
+ * overridden method to get minimum time
+ */
 @Override
 public int getminTimer() {
 	// TODO Auto-generated method stub
 	return minTime;
 }
-
+/*
+ * overridden method to get maximum time
+ */
 @Override
 public int getmaxTimer() {
 	// TODO Auto-generated method stub
--- src/Cycling.java
+++ src/Cycling.java
@@ -1,11 +1,13 @@
 import java.text.DecimalFormat;
 
 public class Cycling extends AbstractEvent {
-static int minTime=500;
-static int maxTime=800;
-static int count =0;
+static int minTime=500;                  //maximum time for event cycling
+static int maxTime=800;                  //minimum time for event cycling
+static int count =0;                     //counting number of times event played
 
- 
+ /*
+  * method to pass minimum and maximum time of event to parent class
+  */
 public Cycling() {
 	super(minTime, maxTime);
 	// TODO Auto-generated constructor stub
@@ -14,10 +16,11 @@ public Cycling() {
 public String getCount(){
 	DecimalFormat formatter = new DecimalFormat("00");
 	String formatted = formatter.format(++count);
-
 	return formatted;
 }
-
+/*
+ * overriding parent method to fetch name of event
+ */
 @Override
 public char getEventName() {
 	// TODO Auto-generated method stub
